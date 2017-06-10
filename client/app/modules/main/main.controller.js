0e01b64873f74799e69fd31f11338eba1f9a557d
ZavgarMaks <m.zavgar@mail.ru>
2017-06-10 15:30:32 +0300

    rooms fixed and working

--- client/app/modules/main/main.controller.js
+++ client/app/modules/main/main.controller.js
@@ -10,12 +10,14 @@
 		$scope.textMsg = '';
 		$scope.userInfo = {};
 		getUserInfo();
+		changeColorActiveRoom();
 		Messages.getMessage(currentRoom)
 			.then(function (response) {
 				$scope.messages = response.data;
+				setTimeout(function () {
+					floatPersonalMsg();
+				}, 10);
 			});
-
-
 		$scope.sendMessages = function () {
 			if ($scope.textMsg !== '') {
 				Messages.createMessage($scope.textMsg, $scope.userInfo.username, getDate(), currentRoom)
@@ -25,8 +27,13 @@
 			}
 		};
 		socket.on('chat message', function (messageInfo) {
-			$scope.messages.push(messageInfo);
-			$scope.$apply();
+			if (messageInfo.room === currentRoom) {
+				$scope.messages.push(messageInfo);
+				$scope.$apply();
+				setTimeout(function () {
+					floatPersonalMsg();
+				}, 10);
+			}
 		});
 
 		$scope.logout = function () {
@@ -36,9 +43,14 @@
 
 		$scope.changeRoom = function (room) {
 			currentRoom = room;
+			changeColorUnactiveRoom();
+			changeColorActiveRoom();
 			Messages.getMessage(currentRoom)
 			.then(function (response) {
 				$scope.messages = response.data;
+				setTimeout(function () {
+					floatPersonalMsg();
+				}, 10);
 			});
 		};
 
@@ -72,6 +84,35 @@
 			}
 			var time = hour + ':' + minute;
 			return time;
+		}
+
+		function changeColorActiveRoom () {
+			var roomName = currentRoom.split('"');
+			var roomNameClass = '.' + roomName + '';
+			var putColorRoom = angular.element(document.querySelector(roomNameClass));
+			putColorRoom.css('background-color', '#0070ba');
+			putColorRoom.css('color', '#fff');
+		}
+
+		function changeColorUnactiveRoom () {
+			var allRooms = angular.element(document.querySelector('.rooms')).find('button');
+			allRooms.css('background-color', 'aliceblue');
+			allRooms.css('color', '#000');
+		}
+		scrollToBottom();
+		function scrollToBottom () {
+			var msgWindow = angular.element(document.querySelector('.messages'));
+			// msgWindow[ 0 ].scrollTop += 800;
+			console.log(document.getElementById('testID').scrollHeight);
+			console.log(msgWindow);
+		}
+
+		function floatPersonalMsg () {
+			var nameClass = '.' + $scope.userInfo.username;
+			var foundedClass = angular.element(document.querySelectorAll(nameClass));
+			var foundedSpan = foundedClass.find('span');
+			foundedClass.css('text-align', 'right');
+			foundedSpan.css('float', 'left');
 		}
 	}
 })();
